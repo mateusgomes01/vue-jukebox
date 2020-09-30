9a4bcdf9bec6d201f81fba33d3ff65c760ec3d40
mateusgomes01 <mgm6@cin.ufpe.br>
2020-09-30 17:12:47 -0300

    Adjusted checkbox

--- src/App.vue
+++ src/App.vue
@@ -9,11 +9,24 @@
             <input type="text" id="surname" name="surname"><br>
             <label for="email">Email:</label><br>
             <input type="text" id="email" name="email"><br>
+            <!--
             <label for="legalPerson">Legal person:</label><br>
-            <input type="text" id="legalPerson" name="legalPerson"><br>
-            <label for="document">CPF/CNPJ:</label><br> <!-- only show CNPJ if Legal person = true. \n
+            <input type="checkbox" id="legalPerson" v-model="legalPerson">
+            <label for="legalPerson">{{ legalPerson }}</label> -->
+            <label for="legalPerson">Legal person? </label>
+            <input
+				type="checkbox"
+				v-model="toggle"
+				true-value="Yes"
+				false-value="No"
+            >
+            <label for="checkbox">{{ toggle }}</label><br>
+            <label for="CPF">CPF:</label><br> <!-- only show CNPJ if Legal person = true. \n
             Use watch to show the desired field-->
-            <input type="text" id="document" name="document"><br>
+            <input type="text" id="CPF" name="CPF"><br>
+            <label for="CNPJ">CNPJ:</label><br> <!-- only show CNPJ if Legal person = true. \n
+            Use watch to show the desired field-->
+            <input type="text" id="CNPJ" name="CNPJ"><br>
         </form>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
     <RegisterTable msg="This will be the register table"/>    
@@ -26,10 +39,40 @@ import RegisterTable from './components/RegisterTable.vue'
 
 export default {
   name: 'App',
+  data() {
+    return {
+		form: {
+			name: 'Mateus Melo'
+		},
+		legalPerson: '',
+		CNPJ: 'nullo',
+		toggle: false,
+    }
+  },
   components: {
     HelloWorld,
     RegisterTable
-  }
+  },
+  watch: {
+    // whenever legalPerson changes, this function will run
+    /*legalPerson(newLegalPerson) {
+      if (legalPerson) {
+        this.showCNPJ();
+      } else {
+        this.hideCNPJ();
+      }
+    }*/
+  },
+  methods: {
+      showCNPJ() {
+        this.CNPJ = '...';
+        this.CNPJ = '12345678';
+      },
+      hideCNPJ() {
+        this.CNPJ = '...';
+        this.CNPJ = 'nullo';
+      }
+    }
 }
 </script>
 
